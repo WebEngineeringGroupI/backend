26f061cb49d7d23b71b57b3588f68fa67d61a021
Fede Barcelona <fede_rico_94@hotmail.com>
2021-10-21 21:15:23 +0200

    refactor(ci): Remove duplicate code from HTTP testing (#4)

--- pkg/application/http/http_test.go
+++ pkg/application/http/http_test.go
@@ -16,78 +16,83 @@ import (
 var _ = Describe("Application / HTTP", func() {
 	var (
 		httpEngine *http.Engine
+		recorder   *httptest.ResponseRecorder
 	)
 
 	BeforeEach(func() {
 		httpEngine = http.NewEngine("http://example.com", inmemory.NewRepository())
+		recorder = httptest.NewRecorder()
 	})
-	Context("when it retrieves an HTTP request for a short URL", func() {
-		It("returns the short URL", func() {
-			handler := httpEngine.Shortener()
-			request := httptest.NewRequest("POST", "http://example.com/api/link", longURLRequest())
-			recorder := httptest.NewRecorder()
-			handler(recorder, request)
-			result := recorder.Result()
 
-			Expect(result.StatusCode).To(Equal(gohttp.StatusOK))
-			Expect(readAll(result.Body)).To(MatchJSON(longURLResponse()))
+	Context("when the handler is the Shortener", func() {
+		var (
+			handler gohttp.HandlerFunc
+		)
+
+		BeforeEach(func() {
+			handler = httpEngine.Shortener()
 		})
-	})
-	Context("when it retrieves an HTTP request for a short URL with malformed JSON key ", func() {
-		It("returns StatusBadRequest code", func() {
-			handler := httpEngine.Shortener()
-			request := httptest.NewRequest("POST", "http://example.com/api/link", badjsonURLRequest())
-			recorder := httptest.NewRecorder()
-			handler(recorder, request)
-			result := recorder.Result()
 
-			Expect(result.StatusCode).To(Equal(gohttp.StatusBadRequest))
+		Context("when it retrieves an HTTP request for a short URL", func() {
+			It("returns the short URL", func() {
+				request := httptest.NewRequest(gohttp.MethodPost, "/api/link", longURLRequest())
 
+				handler(recorder, request)
+				result := recorder.Result()
+
+				Expect(result.StatusCode).To(Equal(gohttp.StatusOK))
+				Expect(readAll(result.Body)).To(MatchJSON(longURLResponse()))
+			})
 		})
-	})
-	Context("when it retrieves an HTTP request for a short URL with malformed long URL", func() {
-		It("returns StatusBadRequest code", func() {
-			handler := httpEngine.Shortener()
-			request := httptest.NewRequest("POST", "http://example.com/api/link", badURLRequest())
-			recorder := httptest.NewRecorder()
-			handler(recorder, request)
-			result := recorder.Result()
 
-			Expect(result.StatusCode).To(Equal(gohttp.StatusBadRequest))
+		Context("when it retrieves an HTTP request for a short URL with malformed JSON key ", func() {
+			It("returns StatusBadRequest code", func() {
+				request := httptest.NewRequest(gohttp.MethodPost, "/api/link", badjsonURLRequest())
+
+				handler(recorder, request)
+
+				result := recorder.Result()
+				Expect(result.StatusCode).To(Equal(gohttp.StatusBadRequest))
+			})
+		})
+
+		Context("when it retrieves an HTTP request for a short URL with malformed long URL", func() {
+			It("returns StatusBadRequest code", func() {
+				request := httptest.NewRequest(gohttp.MethodPost, "/api/link", badURLRequest())
+
+				handler(recorder, request)
+
+				result := recorder.Result()
+				Expect(result.StatusCode).To(Equal(gohttp.StatusBadRequest))
+			})
 		})
 	})
 })
 
 func longURLRequest() io.Reader {
-	return strings.NewReader(`
-{
+	return strings.NewReader(`{
 	"url": "https://google.es"
-}
-`)
+}`)
 }
 
 func longURLResponse() string {
-	return `
-{
+	return `{
 	"url": "http://example.com/lxqrJ9xF"
-}
-`
+}`
 }
 
 func badjsonURLRequest() io.Reader {
 	return strings.NewReader(`
 {
 	"badjson": "https://google.es"
-}
-`)
+}`)
 }
 
 func badURLRequest() io.Reader {
 	return strings.NewReader(`
 {
 	"url": "ftp://google.es"
-}
-`)
+}`)
 }
 
 func readAll(reader io.Reader) string {
