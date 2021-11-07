c007c86d90fb6ba2b9333f2cb7cadb23445409f8
Federico Barcelona <fede_rico_94@hotmail.com>
2021-11-07 16:07:58 +0100

    refactor: Remove unused HTTP schema validator

--- pkg/domain/url/url_validator.go
+++ pkg/domain/url/url_validator.go
@@ -2,22 +2,10 @@ package url
 
 import (
 	"errors"
-	"strings"
 )
 
 var ErrUnableToValidateURL = errors.New("unable to validate URL")
 
 type Validator interface {
 	ValidateURL(url string) (bool, error)
-}
-
-type httpValidator struct{}
-
-func (h *httpValidator) ValidateURL(aLongURL string) (bool, error) {
-	isValidURL := strings.HasPrefix(aLongURL, "http://") || strings.HasPrefix(aLongURL, "https://")
-	return isValidURL, nil
-}
-
-func NewHTTPValidator() Validator {
-	return &httpValidator{}
 }
