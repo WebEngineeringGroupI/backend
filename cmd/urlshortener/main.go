841676914ddb30344fdb94b52814de36b1e75336
German Garces <757024@unizar.es>
2021-11-13 16:35:33 +0100

    feat: added an endpoint to expose application metrics

--- cmd/urlshortener/main.go
+++ cmd/urlshortener/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 )
 
 func main() {
 	factory := newFactory()
+
+	// This two instructions expose application metric on http://localhost:2112/metrics
+	// We can expose our own metrics, more info here https://prometheus.io/docs/guides/go-application/#adding-your-own-metrics
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 
 	log.Fatal(http.ListenAndServe(":8080", factory.NewHTTPRouter()))
 }
