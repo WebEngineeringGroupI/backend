7707c65fc88c3843352af4a1080318db67ece410
Germán Garcés <757024@unizar.es>
2021-12-07 19:59:31 +0100

    fixed: removed total_urls_processeed (#23)

--- pkg/infrastructure/metrics/prometheus.go
+++ pkg/infrastructure/metrics/prometheus.go
@@ -6,18 +6,12 @@ import (
 )
 
 type PrometheusMetrics struct {
-	urlsProcessed         prometheus.Counter
 	singleUrlsProcessed   prometheus.Counter
 	multipleUrlsProcessed prometheus.Counter
 	fileUrlsProcessed     prometheus.Counter
 }
 
 func NewPrometheusMetrics() *PrometheusMetrics {
-	var urlsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "urlshortener_urls_processed_total",
-		Help: "The total number of shorted urls",
-	})
-
 	var singleUrlsProcessed = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "urlshortener_single_urls_processed_total",
 		Help: "The total number of single shorted urls",
@@ -34,15 +28,10 @@ func NewPrometheusMetrics() *PrometheusMetrics {
 	})
 
 	return &PrometheusMetrics{
-		urlsProcessed:         urlsProcessed,
 		singleUrlsProcessed:   singleUrlsProcessed,
 		multipleUrlsProcessed: multipleUrlsProcessed,
 		fileUrlsProcessed:     fileUrlsProcessed,
 	}
-}
-
-func (r *PrometheusMetrics) RecordUrlsProcessed() {
-	r.urlsProcessed.Inc()
 }
 
 func (r *PrometheusMetrics) RecordSingleURLMetrics() {
